3c61f42b2f47340b4bb13edbc1225cfbf4c839bf
Mrsig11 <sis.konan07@gmail.com>
2022-05-26 19:03:19 +0000

    Procfile for Heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Yes Ivoire</title>
     <link rel="stylesheet" href="./static/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-
+    <link rel="icon" href="{{ asset('assets/logo.jpg') }}">
     <style>
         html{
             height: 100%;
